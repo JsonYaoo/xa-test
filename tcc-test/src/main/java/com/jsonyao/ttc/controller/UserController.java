d1aeca53ad63ea88bf00ec2c441f2011f3115268
yaocs2 <yaocs2@midea.com>
2021-02-16 13:27:03 +0800

    xa-test> tcc-test> 分布式接口幂等性: 根据唯一业务号删除可以保证幂等

--- tcc-test/src/main/java/com/jsonyao/ttc/controller/UserController.java
+++ tcc-test/src/main/java/com/jsonyao/ttc/controller/UserController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 分布式接口幂等性: 用户测试服务前端控制器
@@ -19,10 +23,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 查询所有用户
+     * @return
+     */
     @RequestMapping("userList")
     public String userList(ModelMap map){
         List<User> users = userService.getAllUsers();
         map.addAttribute("users", users);
         return "user/user-list";
+    }
+
+    @RequestMapping("delUser")
+    @ResponseBody
+    public Map<String,Object> delUser(@RequestParam Integer userId){
+        int result = userService.delUser(userId);
+        Map<String,Object> map = new HashMap<>();
+        map.put("status",result);
+        return map;
     }
 }
--- tcc-test/src/main/java/com/jsonyao/ttc/service/UserService.java
+++ tcc-test/src/main/java/com/jsonyao/ttc/service/UserService.java
@@ -2,6 +2,7 @@ package com.jsonyao.ttc.service;
 
 import com.jsonyao.ttc.db142.dao.UserMapper;
 import com.jsonyao.ttc.db142.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -11,6 +12,7 @@ import java.util.List;
  * 分布式接口幂等性: 用户测试服务
  */
 @Service
+@Slf4j
 public class UserService {
 
     @Resource
@@ -22,5 +24,20 @@ public class UserService {
      */
     public List<User> getAllUsers() {
         return userMapper.selectAllUsers();
+    }
+
+    /**
+     * 根据UserId删除用户: 根据唯一业务号删除可以保证幂等, 最好删除前先查询一下
+     * @param userId
+     * @return
+     */
+    public int delUser(Integer userId) {
+        User user = userMapper.selectByPrimaryKey(userId);
+        if (user!=null){
+            log.info("用户存在，用户为："+userId);
+            return userMapper.deleteByPrimaryKey(userId);
+        }
+        log.info("用户不存在存在，用户为："+userId);
+        return 0;
     }
 }
